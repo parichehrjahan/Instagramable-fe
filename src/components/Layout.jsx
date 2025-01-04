c3626c5f53f3ec2b3c2a5ee1dea96e105bcb1d77
Parsa Tajik <parsa1379.tajik@gmail.com>
2025-01-03 17:16:35 -0800

    added basic auth

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,15 +1,48 @@
 import { Outlet } from 'react-router'
 import Header from './Header'
+import { useState, useEffect } from 'react'
+import { createClient } from '@supabase/supabase-js'
+import { Auth } from '@supabase/auth-ui-react'
+import { ThemeSupa } from '@supabase/auth-ui-shared'
+
+const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
+)
 
 const Layout = () => {
-  return (
-    <div>
-      <Header />
-      <main className="p-4">
-        <Outlet />
-      </main>
-    </div>
-  )
+  const [session, setSession] = useState(null)
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+    })
+
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+
+    return () => subscription.unsubscribe()
+  }, [])
+
+  if (!session) {
+    return (
+      <div className="mx-auto w-full max-w-2xl mt-[50vh] -translate-y-1/2">
+        <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        <Header />
+        <main className="p-4">
+          <Outlet />
+        </main>
+      </div>
+    )
+  }
 }
 
 export default Layout
