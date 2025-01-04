a50590a683b520d3e54d88b9e9954a96f13621dd
Parsa Tajik <parsa1379.tajik@gmail.com>
2025-01-03 17:58:30 -0800

    added authentication to the app

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,48 +1,29 @@
 import { Outlet } from 'react-router'
 import Header from './Header'
-import { useState, useEffect } from 'react'
-import { createClient } from '@supabase/supabase-js'
 import { Auth } from '@supabase/auth-ui-react'
 import { ThemeSupa } from '@supabase/auth-ui-shared'
-
-const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_ANON_KEY
-)
+import { useUser } from '../contexts/UserContext'
+import { getDB } from '../lib/utils'
 
 const Layout = () => {
-  const [session, setSession] = useState(null)
-
-  useEffect(() => {
-    supabase.auth.getSession().then(({ data: { session } }) => {
-      setSession(session)
-    })
-
-    const {
-      data: { subscription },
-    } = supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session)
-    })
-
-    return () => subscription.unsubscribe()
-  }, [])
+  const { session } = useUser()
 
   if (!session) {
     return (
       <div className="mx-auto w-full max-w-2xl mt-[50vh] -translate-y-1/2">
-        <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />
-      </div>
-    )
-  } else {
-    return (
-      <div>
-        <Header />
-        <main className="p-4">
-          <Outlet />
-        </main>
+        <Auth supabaseClient={getDB()} appearance={{ theme: ThemeSupa }} />
       </div>
     )
   }
+
+  return (
+    <div>
+      <Header />
+      <main className="p-4">
+        <Outlet />
+      </main>
+    </div>
+  )
 }
 
 export default Layout
